06d59cc21a8cfdaad44bbdae095e33358ead5719
Stefan Talpalaru <stefantalpalaru@yahoo.com>
2017-01-06 15:38:28 +0100

    fixes for new projects in directories other than the current one

--- new_project.sh
+++ new_project.sh
@@ -7,22 +7,23 @@ old_name="vala-skeleton"
 old_name_spaces="`echo $old_name | tr '-' ' '`"
 old_name_underscores="`echo $old_name | tr '-' '_'`"
 new_name="$1"
-new_name_spaces="`echo $new_name | tr '-' ' '`"
-new_name_underscores="`echo $new_name | tr '-' '_'`"
+new_name_filename="${new_name##*/}"
+new_name_spaces="`echo $new_name_filename | tr '-' ' '`"
+new_name_underscores="`echo $new_name_filename | tr '-' '_'`"
 
 cp -a "$old_name" "$new_name"
-git clean -fXdq "${new_name}/"
-git clean -fXdq "${new_name}/src/"
-git clean -fXdq "${new_name}/po/"
-git clean -fXdq "${new_name}/data/"
-mv "${new_name}/data/${old_name}.svg" "${new_name}/data/${new_name}.svg"
-mv "${new_name}/data/${old_name}.desktop.in.in" "${new_name}/data/${new_name}.desktop.in.in"
-mv "${new_name}/${old_name}-9999.ebuild" "${new_name}/${new_name}-9999.ebuild"
-sed -i -e "s/$old_name/$new_name/g" "${new_name}/.gitignore"
-sed -i -e "s/$old_name/$new_name/g" -e "s/$old_name_spaces/$new_name_spaces/g" "${new_name}/configure.ac"
-sed -i -e "s/$old_name/$new_name/g" -e "s/$old_name_underscores/$new_name_underscores/g" "${new_name}/Makefile.am"
+cd "$new_name"
+git init &>/dev/null
+git clean -fXdq
+cd - &>/dev/null
+mv "${new_name}/data/${old_name}.svg" "${new_name}/data/${new_name_filename}.svg"
+mv "${new_name}/data/${old_name}.desktop.in.in" "${new_name}/data/${new_name_filename}.desktop.in.in"
+mv "${new_name}/${old_name}-9999.ebuild" "${new_name}/${new_name_filename}-9999.ebuild"
+sed -i -e "s%$old_name%$new_name_filename%g" "${new_name}/.gitignore"
+sed -i -e "s%$old_name%$new_name_filename%g" -e "s%$old_name_spaces%$new_name_spaces%g" "${new_name}/configure.ac"
+sed -i -e "s%$old_name%$new_name_filename%g" -e "s%$old_name_underscores%$new_name_underscores%g" "${new_name}/Makefile.am"
 sed -i -e "2d" "${new_name}/po/LINGUAS"
-sed -i -e "s/$old_name/$new_name/g" "${new_name}/po/POTFILES.in"
+sed -i -e "s%$old_name%$new_name_filename%g" "${new_name}/po/POTFILES.in"
 rm "${new_name}/po/it.po"
 
 cat <<END
@@ -30,15 +31,15 @@ cat <<END
 The "$new_name" directory was created. You can now move it somewhere else.
 Edit the first line of "${new_name}/configure.ac" according to
 http://www.gnu.org/software/autoconf/manual/autoconf.html#Initializing-configure ,
-change the icon in "${new_name}/data/${new_name}.svg" to something original,
-change the program's menu description  in "${new_name}/data/${new_name}.desktop.in.in"
+change the icon in "${new_name}/data/${new_name_filename}.svg" to something original,
+change the program's menu description  in "${new_name}/data/${new_name_filename}.desktop.in.in"
 and put the license of your choice in "${new_name}/COPYING".
 END
 
 [ -f /etc/gentoo-release ] && cat <<END
 
-To use the ebuild during development, copy it to your local overlay, create a git repo for your new
-project directory and put its full path in the EGIT_REPO_URI variable. Remember to add the new package to
-/etc/portage/package.keywords .
+To use the ebuild during development, copy it to your local overlay and put
+your new git repo's full path in the EGIT_REPO_URI variable. Remember to add
+the new package to /etc/portage/package.keywords .
 END
 
